a0eee0f485ede8de3f02bc5ee3ecfeaecff2860f
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 16:31:13 +0800

    print bigger number code

--- print_bigger_num.py
+++ print_bigger_num.py
@@ -5,4 +5,4 @@ num2=float(input("Please enter second number: "))
 if num1>num2:
     print(f"{num1} is bigger than {num2}")
 else:
-    print(f"{num2} is biger than {num1}")
+    print(f"{num2} is bigger than {num1}")
