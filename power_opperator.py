391ce00a387f093343985e8e20074e65099c6883
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 22:27:37 +0800

    changes

--- power_opperator.py
+++ power_opperator.py
@@ -1,4 +1,4 @@
 # input two numbers and print the result when the first number is raised to the second number
-print(f"The result is:",
+print(f"The result is: ",
 float(input("Enter the first number:")) **
 float(input("Enter the second number:")))
