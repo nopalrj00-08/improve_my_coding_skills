8e6eaec8e7a9691af759b88aab5870c0c4d2056c
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 17:57:54 +0800

    changes

--- product_of _two _numbers.py
+++ product_of _two _numbers.py
@@ -1,5 +1,4 @@
-#The product of two numbers
+# the product of two numbers
 print(f"The product of two numbers is ",
 float(input("Enter the first number:")) *
 float(input("Enter the second number:")))
-
