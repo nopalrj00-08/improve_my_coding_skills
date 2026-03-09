7ab2cfa6fd9dbad9281e8cd7d0c52c39fc48f277
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-10 00:22:41 +0800

    find the smaller number

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,2 +1,7 @@
 # input two numbers
-# print the smaller number
+num_1=float(input("Please enter first number:"))
+num_2=float(input("Please enter second number:"))
+if num_1<num_2: # find the smaller number and print it
+    print(f"{num_1} is smaller than {num_2}.")
+else:
+    print(f"{num_2} is smaller than {num_1}.")
