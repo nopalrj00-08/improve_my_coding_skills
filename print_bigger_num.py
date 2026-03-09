ce5b304c66accc9486e35beaeadc34a84983c32b
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 16:24:23 +0800

    input codes

--- print_bigger_num.py
+++ print_bigger_num.py
@@ -1,2 +1,8 @@
 # input 2 numbers
+num1=float(input("Please enter first number: "))
+num2=float(input("Please enter second number: "))
 # print bigger number
+if num1>num2:
+    print(f"{num1} is bigger than {num2}")
+else:
+    print(f"{num2} is biger than {num1}")
