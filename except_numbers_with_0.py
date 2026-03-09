b83853c0129c5c185769f7f7e85863d1993c9ea7
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 23:52:38 +0800

    remove numbers with 0's

--- except_numbers_with_0.py
+++ except_numbers_with_0.py
@@ -1,3 +1,4 @@
 # input 100 numbers
-# not include the numbers with 0's
-# print the numbers
+for i in range (0,101):
+    if i % 10 !=0: # not include the numbers with 0's
+        print(f"{i}",end=",") # print the numbers
