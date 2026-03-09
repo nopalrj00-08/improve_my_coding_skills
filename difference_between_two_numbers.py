92bf06a361d522d713f27bc7adbf85e066942ef8
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-10 01:24:57 +0800

    difference between two numbers

--- difference_between_two_numbers.py
+++ difference_between_two_numbers.py
@@ -1,2 +1,4 @@
-# input two numbers
 # print the difference between two numbers
+print("The difference is : ",
+float(input("Enter first number:")) - # input two numbers
+float(input("Enter second number:"))) # input two numbers
