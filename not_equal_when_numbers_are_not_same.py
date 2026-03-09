3d6fd470ab732973d55ef8da619fd02c8451c0f1
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-10 01:16:55 +0800

    Not Equal

--- not_equal_when_numbers_are_not_same.py
+++ not_equal_when_numbers_are_not_same.py
@@ -1,2 +1,7 @@
 # input two numbers
-# print when numbers are not equal
+num_1=float(input("Please enter first number:"))
+num_2=float(input("Please enter second number:"))
+if num_1 != num_2:
+    print(f"{num_1} is not equal to {num_2}.") # print when numbers are not equal
+else:
+    print(f"{num_1} is equal to {num_2}.")
