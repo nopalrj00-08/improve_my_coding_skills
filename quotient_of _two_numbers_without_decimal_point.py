36630538eb84bf39bb585498ef5c00efee2f4420
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-10 01:38:39 +0800

    quotient without decimal point

--- quotient_of _two_numbers_without_decimal_point.py
+++ quotient_of _two_numbers_without_decimal_point.py
@@ -1,2 +1,5 @@
-# input two numbers
 # print the quotient of two numbers without decimal point
+print(f"The quotient is: ",
+int(input("Enter first number:")) // # input two numbers
+int(input("Enter second number:"))) # input two numbers
+
