9bf81f166c1e01fcd511b96383889339ed9a27cd
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 17:01:37 +0800

    input codes

--- print_equal_when_the_numbers_are_the_same.py
+++ print_equal_when_the_numbers_are_the_same.py
@@ -1,2 +1,5 @@
 #input two numbers
+number_1=float(input("Please enter first number:"))
+number_2=float(input("Please enter second number:"))
 #print equal if numbers are the same
+print(f"{number_1} and {number_2} are equal")if number_1 == number_2 else print(f"{number_1} and {number_2} are not equal")
