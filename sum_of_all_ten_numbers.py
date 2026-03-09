a74dd282f4a117b01815b0f3ad86681cd8b4cbcd
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 22:55:24 +0800

    changes

--- sum_of_all_ten_numbers.py
+++ sum_of_all_ten_numbers.py
@@ -1,5 +1,5 @@
 numbers = 0
 for i in range(10): # ask for 10 numbers
-    num=float(input("Enter a number:")) # input
+    num=float(input("Enter a number: ")) # input
     numbers += num # print the sum of 10 numbers
 print(f"The sum of all ten numbers is {numbers}")
