e34041752a23cfbcfc6bdb511ce2402b9c3b2019
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-10 01:47:20 +0800

    remainder of two divided numbers

--- remainder_when_two_numbers_are_divided.py
+++ remainder_when_two_numbers_are_divided.py
@@ -1,2 +1,4 @@
-# input two numbers
 # print the remainder or two numbers
+print("The Remainder is: ",
+int(input("Enter first number:")) % #input two numbers
+int(input("Enter second number:"))) # input two numbers
