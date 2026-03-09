be457f953625565f17dd587b229ffbbaeed3928d
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 22:53:20 +0800

    counting odd numbers input codes

--- odds_in_ten_numbers.py
+++ odds_in_ten_numbers.py
@@ -1,3 +1,8 @@
-# input ten numbers
-# print how many are odds
-# counting the odd numbers
+count = 0
+for i in range (10):
+    num=float(input("Enter Ten numbers:  ")) # input ten numbers
+    if num % 2 != 0:
+        count += 1 # counting the odd numbers
+print(f"The odd numbers are {count}") # print how many are odds
+
+
