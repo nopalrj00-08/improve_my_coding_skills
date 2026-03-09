f48b520216e379e689c152448f376f350b0a792a
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-09 23:27:46 +0800

    print even number 0-100

--- even_numbers_from_0_to_100.py
+++ even_numbers_from_0_to_100.py
@@ -1,3 +1,4 @@
-# input 100 numbers
-# print the even numbers
-# count the even numbers
+# for loop 100 numbers
+for i in range(0,101):
+     if i % 2 == 0: # even numbers 
+         print(f"{i}",end=",") # print the even numbers
