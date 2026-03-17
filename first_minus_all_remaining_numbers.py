4414c4080a6da37d6e78e1861ac03154ebcb9070
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-17 20:43:03 +0800

    codes first

--- first_minus_all_remaining_numbers.py
+++ first_minus_all_remaining_numbers.py
@@ -1,4 +1,11 @@
 # input ten numbers
 # input
+result = 0
+for i in range(9):
+    for j in range(1):
+        num1 = float(input("Enter a number: "))
+        num = float(input("Enter Ten numbers:"))
+        result = num1 - num
+print(result)
 # minus the first number to the remaining numbers
 # print the result
