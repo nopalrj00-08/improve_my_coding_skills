60683f58a8b3eeeda6d3d88cdb791661344898ed
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-18 23:32:57 +0800

    print even numbers

--- ask_for_10_and _find _even_numbers.py
+++ ask_for_10_and _find _even_numbers.py
@@ -1,3 +1,6 @@
-# Ask for 10 numbers
-# Find the even numbers
-# print the even numbers
+numbers = 0
+for i in range (10):
+    num=float(input(f"Enter a number {i+1}: ")) # Ask for 10 numbers
+    if num % 2 == 0: # Find the even numbers
+        numbers += 1
+print(f"{numbers} even numbers") # Print the even numbers
