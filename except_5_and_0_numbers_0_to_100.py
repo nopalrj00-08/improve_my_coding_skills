0cb66892008e07b54fbc8646a91755e055e32319
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-19 00:52:22 +0800

    codes

--- except_5_and_0_numbers_0_to_100.py
+++ except_5_and_0_numbers_0_to_100.py
@@ -1,3 +1,3 @@
-# 0 to 100
-# Except numbers with 5 and 0
-# Print
+for i in range (100): # 0 to 100
+    if i % 10 != 0 and i % 5 != 0: # Except numbers with 5 and 0
+        print(i,end=",") # Print
