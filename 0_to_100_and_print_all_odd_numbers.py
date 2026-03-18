82a3166aff0417f36a84932ed56e257252f1fbc3
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-03-19 00:21:13 +0800

    0 - 100 odd only

--- 0_to_100_and_print_all_odd_numbers.py
+++ 0_to_100_and_print_all_odd_numbers.py
@@ -1,3 +1,5 @@
-# 0 to 100 numbers
-# Find the od numbers
-# Print all odd numbers from 0 to 100
+i=0
+while i <= 100: # 0 to 100 numbers
+    if i % 2 != 0: # Find the od numbers
+        print(f"{i}",end=",") # Print all odd numbers from 0 to 100
+    i += 1
